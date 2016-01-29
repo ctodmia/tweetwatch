738bcad0afac9e56663dbce2388ffe474965cc12
Carine TOdmia <ctodmia@gmail.com>
2016-01-29 11:31:51 -0800

    feat: express initialized, serving index.html as a static file

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-server.js
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/client'));
+
+app.listen(port);
+console.log('its going down on port ' + port);
