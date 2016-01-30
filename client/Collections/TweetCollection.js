39df7695a8d1efe78ca81c02cbcf2ea3efbb02c3
Carine TOdmia <ctodmia@gmail.com>
2016-01-30 12:52:33 -0800

    edit: console.logs removed, dynamically get user names through the console.

--- client/Collections/TweetCollection.js
+++ client/Collections/TweetCollection.js
@@ -1,7 +1,6 @@
 var TweetCollection = Backbone.Collection.extend({
 	model: Tweet,
 	url: function(name) {
-		console.log('this the name you entered', name);
 		return '/tweets/'+name;
 	},
 	parse: function(data) {
